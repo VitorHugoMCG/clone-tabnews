1474a25ccf6603ebb9a46582d62aa810b7734028
Vitor Hugo Marcondes <95134073+VitorHugoMCG@users.noreply.github.com>
2026-02-09 21:12:39 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Eu vou conseguir finalizar o curso!</h1>
+}
+
+//exportar essa funcao para fora do arquivo, default é o padrao (a parti dele) 
+export default Home;

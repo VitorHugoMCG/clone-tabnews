5a3209f56acecd0e8982ca2ccbc600ce06b91fed
Vitor Hugo Marcondes <95134073+VitorHugoMCG@users.noreply.github.com>
2026-03-16 13:32:07 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h1>Eu vou conseguir finalizar o curso.dev!!!</h1>
+  return <h1>Eu vou conseguir finalizar o curso.dev!!!</h1>;
 }
 
-//exportar essa funcao para fora do arquivo, default é o padrao (a parti dele) 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
+}
+
+//exportar essa funcao para fora do arquivo, default é o padrao (a parti dele)
 export default Home;
